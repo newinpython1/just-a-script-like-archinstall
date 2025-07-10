ae2c5abedd4691c7cdce023ed7b0e7728d56950f
newinpython1 <matfejkrutoj@gmail.com>
2025-07-10 23:42:51 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "enter your name:"
-read name
-echo "enter your passwd:"
-read passwd
 mkfs.fat -F32 /dev/sda1 && mkfs.ext4 /dev/sda2 && mkfs.ext4 /dev/sda3 && mkswap /dev/sda4 && swapon /dev/sda4 && mount /dev/sda1 /boot/efi && mount /dev/sda2 /mnt && mount /dev/sda3 /mnt && pacstrap -K /mnt base-devel base fastfetch linux linux-firmware sudo nano && genfstab -U /mnt >> /mnt/etc/fstab # i will add new file for you start it on arch-chroot /mnt
