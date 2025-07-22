5dc3071cfa8e8abe0f53c2e2ea5a03eb66d1db48
newinpython1 <matfejkrutoj@gmail.com>
2025-07-22 14:27:20 +0500

    Update archchroot.sh

--- archchroot.sh
+++ archchroot.sh
@@ -4,4 +4,4 @@ echo "enter your name:"
 read name
 echo "enter your password:"
 read password
-useradd -m -G wheel $name && passwd && sudo pacman -S grub efibootmgr os-prober mtools && $passwd && mkdir /boot/efi && mount /dev/sda1 /boot/efi && grub-install --target=x86_64-efi --bootloader-id=grub_uefi && grub-mkconfig -o /boot/grub/grub.cfg && exit && umount -R /mnt && reboot
+useradd -m -G wheel $name && passwd && sudo pacman -S grub efibootmgr os-prober mtools && $passwd && mkdir /boot/efi && mount /dev/sda1 /boot/efi && grub-install --target=x86_64-efi --bootloader-id=grub_uefi && grub-mkconfig -o /boot/grub/grub.cfg
